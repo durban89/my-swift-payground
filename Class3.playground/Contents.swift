9ae7b85f5a8863f422842929ba04b489b0aa4513
durban.zhang <durban.zhang@gmail.com>
2017-05-15 09:31:53 +0800

    add class Optional

--- Class3.playground/Contents.swift
+++ Class3.playground/Contents.swift
@@ -6,6 +6,9 @@ class Person {
     var age = 20
     var name = "Durban"
     
+    var address:String?
+    var phone:String?
+    
     init(_ name:String, _ age:Int){
         self.name = name
         self.age = age
@@ -20,6 +23,20 @@ var a = Person("Durban2",30)
 print(a.name)
 print(a.age)
 
+a.address = "shanghai"
+
+if let optionalAddress = a.address{
+    print("where shanghai")
+}
+if a.address != nil{
+    print("where \(String(describing: a.address))")
+}
+
+if a.address == nil {
+    print("no address")
+}
+
 var b = Person()
 print(b.name)
 print(b.age)
+
