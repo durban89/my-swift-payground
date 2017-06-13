c1de2159edd931b7c82c36069be33fbeb4c98af9
durban.zhang <durban.zhang@gmail.com>
2017-06-13 22:33:33 +0800

    update - practise optional values

--- ControlFlow.playground/Contents.swift
+++ ControlFlow.playground/Contents.swift
@@ -14,3 +14,17 @@ for score in individualScores{
 }
 
 print(teamScore)
+
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "Durban.zhang"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+let nickName: String? = nil
+let fullName: String = "Durban.zhang"
+let informalGreeting = "Hi, \(nickName ?? fullName)"
