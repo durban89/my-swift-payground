f86a4146396988c130d57e8796f9d66c6c762403
durban.zhang <durban.zhang@gmail.com>
2017-06-15 22:43:56 +0800

    practise while for

--- ControlFlow.playground/Contents.swift
+++ ControlFlow.playground/Contents.swift
@@ -59,3 +59,29 @@ for (kind, numbers) in interestingNumbers {
 }
 
 print(largest)
+
+
+// while
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+
+
+
+
+
+
