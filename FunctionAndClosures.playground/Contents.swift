8960cb8769e335fb9b27d4de24098958e55ca46c
durban.zhang <durban.zhang@gmail.com>
2017-06-17 22:12:30 +0800

    practise define function label and complex value return multi value

--- FunctionAndClosures.playground/Contents.swift
+++ FunctionAndClosures.playground/Contents.swift
@@ -14,3 +14,33 @@ func greet(_ person:String, _ day: String) -> String {
 }
 
 greet("durban", "Thusday")
+
+func greet2(_ person: String, on day:String) -> String{
+    return "Hello \(person), today is \(day)"
+}
+
+greet2("durban",on: "Thusday")
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int){
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        
+        sum += score
+    }
+    
+    return (min, max, sum)
+    
+}
+
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.min)
+print(statistics.max)
+print(statistics.sum)
