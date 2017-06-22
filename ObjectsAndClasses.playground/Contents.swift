5cd7e9f147d6e934932ec9bd239b755e6bc81c89
durban.zhang <durban.zhang@gmail.com>
2017-06-22 09:21:21 +0800

    add class init and override practise

--- ObjectsAndClasses.playground/Contents.swift
+++ ObjectsAndClasses.playground/Contents.swift
@@ -13,3 +13,41 @@ var shape = Shape()
 shape.numberOfSides = 7
 var shapeDescription = shape.simpleDescription()
 print(shapeDescription)
+
+class NamedShape{
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+// override func
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String){
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let testSquare = Square(sideLength: 5.2, name: "my test square")
+testSquare.area()
+testSquare.simpleDescription()
+
+
