d14336feb9f69c51ccb30f2dcc83d9ce021377c8
durban.zhang <durban.zhang@gmail.com>
2017-06-23 09:16:00 +0800

    add class getter and setter func

--- ObjectsAndClasses.playground/Contents.swift
+++ ObjectsAndClasses.playground/Contents.swift
@@ -50,4 +50,34 @@ let testSquare = Square(sideLength: 5.2, name: "my test square")
 testSquare.area()
 testSquare.simpleDescription()
 
+// class getter and setter
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var primeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)"
+    }
+    
+}
 
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.primeter)
+triangle.primeter = 9.9
+print(triangle.sideLength)
+print(triangle.simpleDescription())
