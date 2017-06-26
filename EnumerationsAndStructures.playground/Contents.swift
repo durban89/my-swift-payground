ef954a40a9c6bba6718bf2155753aa5c0d8c77c9
durban.zhang <durban.zhang@gmail.com>
2017-06-26 09:27:56 +0800

    practise enum

--- EnumerationsAndStructures.playground/Contents.swift
+++ EnumerationsAndStructures.playground/Contents.swift
@@ -2,4 +2,33 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    func simpleDescription () -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+let two = Rank.two
+let twoRawValue = two.rawValue
+two.simpleDescription()
+
+let jack = Rank.jack
+let jackRawValue = jack.rawValue
+jack.simpleDescription()
