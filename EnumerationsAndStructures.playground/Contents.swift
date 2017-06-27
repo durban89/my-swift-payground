592edca69032cea5a72978551a9ac36579f70939
durban.zhang <durban.zhang@gmail.com>
2017-06-27 09:30:00 +0800

    add enum  two value

--- EnumerationsAndStructures.playground/Contents.swift
+++ EnumerationsAndStructures.playground/Contents.swift
@@ -32,3 +32,38 @@ two.simpleDescription()
 let jack = Rank.jack
 let jackRawValue = jack.rawValue
 jack.simpleDescription()
+
+
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartDescription = hearts.simpleDescription()
+
+//
+enum ServerResponse {
+    case result (String, String)
+    case failure (String)
+}
+
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out of cheese.")
+
+switch failure {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+    print("Failure... \(message).")
+}
