d3a32ff4165b66f8ca060cfba7937eac307ba787
durban.zhang <durban.zhang@gmail.com>
2017-06-28 09:18:18 +0800

    add protocol

---
+++ ProtocolsAndExtensions.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// use Protocol to declare a protocol
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
