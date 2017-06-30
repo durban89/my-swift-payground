9ed6361b5dd0b5bbfae735cb1fdcf92d6cfcbce4
durban.zhang <durban.zhang@gmail.com>
2017-06-30 13:53:04 +0800

    add extension to add functionality for a type

--- ProtocolsAndExtensions.playground/Contents.swift
+++ ProtocolsAndExtensions.playground/Contents.swift
@@ -32,3 +32,17 @@ var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
 print(bDescription)
+
+// use extension to add functionality to an existing type
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    
+    mutating func adjust() {
+        self += 42
+    }
+}
+
+print(7.simpleDescription)
