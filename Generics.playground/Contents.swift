cfb2dc01ef04db6d1645d9070920ac59275e1221
durban.zhang <durban.zhang@gmail.com>
2017-07-06 21:36:34 +0800

    add generic where

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -22,3 +22,19 @@ var possibleInteger: OptionalValue<Int> = .none
 print(possibleInteger)
 possibleInteger = .some(100)
 print(possibleInteger)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs:T, _ rhs: U) -> Bool
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        
+        return false
+}
+
+anyCommonElements([1,2,3], [3])
